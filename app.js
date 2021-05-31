119dcbc2974b55a8980b32b71d365cab6e914088
Aleksandar Beric <beric.aleksandar@gmail.com>
2021-05-31 16:10:03 +0200

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Zdravo svete!'));
+app.get('/', (req, res) => res.send('Zdravo svete jos jednom!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
